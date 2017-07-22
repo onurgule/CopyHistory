fe1abc48981cd42568de3ff0254ea276d4bb1969
Onur Osman Gule <m2.hesaplar@hotmail.com>
2017-07-22 06:42:10 +0300

    Copy History

--- CopyHistory/frmMain.Designer.cs
+++ CopyHistory/frmMain.Designer.cs
@@ -28,8 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.listBox = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.CopyHistory = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.Hide = new System.Windows.Forms.ToolStripMenuItem();
+            this.Close = new System.Windows.Forms.ToolStripMenuItem();
+            this.CopyHistory.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox
@@ -51,6 +59,48 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Coded by Onurgule";
             // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.CopyHistory;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // CopyHistory
+            // 
+            this.CopyHistory.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.CopyHistory.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Open,
+            this.Hide,
+            this.Close});
+            this.CopyHistory.Name = "CopyHistory";
+            this.CopyHistory.Size = new System.Drawing.Size(176, 104);
+            // 
+            // Open
+            // 
+            this.Open.AccessibleName = "tOpen";
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(175, 24);
+            this.Open.Text = "Open";
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Hide
+            // 
+            this.Hide.AccessibleName = "tHide";
+            this.Hide.Name = "Hide";
+            this.Hide.Size = new System.Drawing.Size(175, 24);
+            this.Hide.Text = "Hide";
+            this.Hide.Click += new System.EventHandler(this.Hide_Click);
+            // 
+            // Close
+            // 
+            this.Close.AccessibleName = "tClose";
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(175, 24);
+            this.Close.Text = "Close";
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -59,12 +109,15 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmMain";
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CopyHistory";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.CopyHistory.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,6 +127,11 @@
 
         private System.Windows.Forms.ListBox listBox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip CopyHistory;
+        private System.Windows.Forms.ToolStripMenuItem Open;
+        private System.Windows.Forms.ToolStripMenuItem Hide;
+        private System.Windows.Forms.ToolStripMenuItem Close;
     }
 }
 
--- CopyHistory/frmMain.cs
+++ CopyHistory/frmMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -22,62 +23,136 @@ namespace CopyHistory
         {
             Hook.CreateHook(KeyReaderr);
         }
-        
-        public static String temp = "";
+        ArrayList cpy = new ArrayList();
+        ArrayList obj = new ArrayList();
+        public static String temp = "zzzzzzz";
         void KeyReaderr(IntPtr wParam, IntPtr lParam)
         {
-            int key = Marshal.ReadInt32(lParam);
-
-            Hook.VK vk = (Hook.VK)key;
-            //MessageBox.Show(vk.ToString());
-            //
-
-            #region
-
-            if(vk == Hook.VK.VK_LCONTROL)
+            try
             {
-                temp += "Co_";
-            }
-            if (vk == Hook.VK.VK_C)
-            {
-                temp += "_pY";
-                if (temp.Length >= 6)
+                int key = Marshal.ReadInt32(lParam);
+
+                Hook.VK vk = (Hook.VK)key;
+                //MessageBox.Show(vk.ToString());
+                //
+
+                #region
+
+                if (vk == Hook.VK.VK_LCONTROL || vk == Hook.VK.VK_RCONTROL)
                 {
-
-                    if (temp.Substring(temp.Length - 6, 6) == "Co__pY")
+                    temp += "Co_";
+                }
+                if (vk == Hook.VK.VK_C || vk == Hook.VK.VK_X || vk == Hook.VK.VK_SNAPSHOT)
+                {
+                    if (temp.Substring(temp.Length - 3, 3) == "Co_") temp += "_pY";
+                    if (vk == Hook.VK.VK_SNAPSHOT) temp = temp + "zzzzzzCo__pY";
+                    if (temp.Length >= 6)
                     {
-                        if (!listBox.Items.Contains(Clipboard.GetText()))
+                        System.Threading.Thread.Sleep(400);
+                        if (temp.Substring(temp.Length - 6, 6) == "Co__pY")
                         {
-                            listBox.Items.Add(Clipboard.GetText());
+                            if (!listBox.Items.Contains(Clipboard.GetDataObject()))
+                            {
+                                string name = "";
+                                Image itemimage = null;
+                                string itemtext = null;
+                                IDataObject data = Clipboard.GetDataObject();
+                                obj.Add(data);
+                                if (data.GetDataPresent(DataFormats.StringFormat))
+                                {
+                                    itemtext = Clipboard.GetText();
+                                    cpy.Add(itemtext);
+                                    listBox.Items.Add(DateTime.Now.ToShortTimeString() + " "+ itemtext);
+
+
+                                }
+                                else
+                                {
+                                    Bitmap bitmap = (Clipboard.GetData(DataFormats.Bitmap) as Bitmap);
+                                    itemimage = bitmap;
+                                    cpy.Add(itemimage);
+                                    listBox.Items.Add(DateTime.Now.ToShortTimeString() + " " + "Image");
+                                }
+
+
+
+
+
+                            }
                         }
                     }
                 }
-            }
-            
 
+            }
+            catch(Exception a)
+            {
+
+            }
            
 
             #endregion
 
            // MessageBox.Show(temp);
         }
-
+        bool closing = false;
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            Hook.DestroyHook();
+            if(closing==false) e.Cancel = true;
+            else
+            {   }
+            this.Hide();
+            this.Visible = false;
+
+            
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            DialogResult yesNo = MessageBox.Show(Convert.ToString(listBox.Items[listBox.SelectedIndex]), "Copy", MessageBoxButtons.YesNo);
+            string st = Convert.ToString(listBox.Items[listBox.SelectedIndex]);
+            DialogResult yesNo = MessageBox.Show(st.Substring(6, st.Length-6), "Copy", MessageBoxButtons.YesNoCancel);
             if (yesNo == DialogResult.Yes)
             {
-                Clipboard.SetText(Convert.ToString(listBox.Items[listBox.SelectedIndex]));
+                if(Convert.ToString(st.Substring(6, st.Length - 6)) == "Image")
+                {
+                    Clipboard.SetData(DataFormats.Bitmap, cpy[listBox.SelectedIndex]);
+                }
+                else
+                {
+                    
+                    Clipboard.SetText(st.Substring(5, st.Length-5));
+
+                }
             }
             else
             {
 
             }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Hide_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.Visible = false;
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            closing = true;
+            Application.Exit();
         }
     }
 }
