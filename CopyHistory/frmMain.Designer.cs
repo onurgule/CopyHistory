97d8865f8624b37d1e0eef99b281c6875e4c3e56
Onur Osman Gule <m2.hesaplar@hotmail.com>
2017-07-22 05:22:23 +0300

    CopyHistory
    It keeps your CTRL+C history.
    Only CTRL+C, not clicking copy or CTRL+V.

---
+++ CopyHistory/frmMain.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CopyHistory
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Location = new System.Drawing.Point(21, 29);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(795, 260);
+            this.listBox.TabIndex = 0;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(685, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Coded by Onurgule";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 317);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CopyHistory";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CopyHistory/frmMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopyHistory
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hook.CreateHook(KeyReaderr);
+        }
+        
+        public static String temp = "";
+        void KeyReaderr(IntPtr wParam, IntPtr lParam)
+        {
+            int key = Marshal.ReadInt32(lParam);
+
+            Hook.VK vk = (Hook.VK)key;
+            //MessageBox.Show(vk.ToString());
+            //
+
+            #region
+
+            if(vk == Hook.VK.VK_LCONTROL)
+            {
+                temp += "Co_";
+            }
+            if (vk == Hook.VK.VK_C)
+            {
+                temp += "_pY";
+                if (temp.Length >= 6)
+                {
+
+                    if (temp.Substring(temp.Length - 6, 6) == "Co__pY")
+                    {
+                        if (!listBox.Items.Contains(Clipboard.GetText()))
+                        {
+                            listBox.Items.Add(Clipboard.GetText());
+                        }
+                    }
+                }
+            }
+            
+
+           
+
+            #endregion
+
+           // MessageBox.Show(temp);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Hook.DestroyHook();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DialogResult yesNo = MessageBox.Show(Convert.ToString(listBox.Items[listBox.SelectedIndex]), "Copy", MessageBoxButtons.YesNo);
+            if (yesNo == DialogResult.Yes)
+            {
+                Clipboard.SetText(Convert.ToString(listBox.Items[listBox.SelectedIndex]));
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
